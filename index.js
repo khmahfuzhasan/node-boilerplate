784953305a5c240c625f17beca85620426fedc11
khmahfuzhasan <khandakermahfuzhasan@gmail.com>
2022-10-29 13:46:47 +0600

    Node Boilerplate

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * Title:  Main App
+ * Description: Utility library for main-app-related functions
+ * Author: Khandaker Mahfuz Hasan
+ * Date: 10/29/2022
+ *
+ */
+
+// Dependencies
+const _ = require('underscore');
